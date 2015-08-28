272b257cdbcc001d04dcd71c9e1a6b2e837de5e9
Hannah Chu (local) <hanchu@cisco.com>
2015-08-27 18:54:54 -0700

    really derpy collisions

--- derps/Box.js
+++ derps/Box.js
@@ -1,4 +1,5 @@
 var things = new Array();
+var guns = new Array();
 var dragging = false;
 var dragOffX;
 var dragOffY;
@@ -18,10 +19,13 @@ var MULTIPLIER = 500;
 var FRICTION = 5;
 var CHAOS = 1000;
 
+var FLOORLEVEL;
+
 var main = function()
 {
 	console.log("Entered main.");
 	var canvas = document.getElementById("canvas");
+	FLOORLEVEL = canvas.height;// - 100;
 	
 	// Get rid of annoying text select
 	canvas.addEventListener("selectstart", function(e)
@@ -29,6 +33,26 @@ var main = function()
 		e.preventDefault();
 		return false;
 	}, false);
+	
+	// Click to fire
+	canvas.addEventListener("click", function(e)
+	{
+		var x = e.pageX - canvas.offsetLeft;
+		var y = e.pageY - canvas.offsetTop;
+		var found;
+		for (var i in guns)
+		{
+			if (guns[i].contains(x, y))
+			{
+				found = guns[i];
+				break;
+			}
+		}
+		if (found)
+		{
+			found.launch((x - found.x) * 50, (y - found.y) * 50)
+		}
+	});
 	
 	// Look if you're clicking on something, start dragging
 	canvas.addEventListener("mousedown", function(e)
@@ -71,6 +95,22 @@ var main = function()
 		update();
 	});
 	
+	// If mouse moves outside
+	canvas.addEventListener("mouseout", function(e)
+	{
+		if (!dragging || currentThing == null)
+			return;
+		var x = e.pageX - canvas.offsetLeft;
+		var y = e.pageY - canvas.offsetTop;
+		
+		// Get the last mouse velocity
+		currentThing.vX = 100 * (x - mouseX) / ((new Date()).getTime() - mouseTime);
+		currentThing.vY = 100 * (y - mouseY) / ((new Date()).getTime() - mouseTime);
+		dragging = false;
+		currentThing = null;
+	});
+	
+	
 	// Release the thing, stop drawing?
 	canvas.addEventListener("mouseup", function(e)
 	{
@@ -96,10 +136,11 @@ var main = function()
 		color += Math.floor((Math.random() * 200 + 55)) + ",";
 		color += Math.floor((Math.random() * 200 + 55)) + ",0.8)";
 		console.log("Added new thing at " + x + " " + y);
-		things.push(new Thing(x, y, 20, color));
+		things.push(new Thing(x, y, 20, color, true));
 		update();
 	});
 	
+	guns.push(new Gun(canvas.width/2, FLOORLEVEL/2, 30));	
 	setInterval(function() { collisionCheck(); }, INTERVAL);
 }
 
@@ -107,12 +148,23 @@ function update()
 {
 	var ctx = getContext();
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.strokeStyle = "rgb(0,0,0)";
+	var path = new Path2D();
+	path.moveTo(0, FLOORLEVEL);
+	path.lineTo(canvas.width, FLOORLEVEL);
+	ctx.stroke(path);
+
 	for (var i in things)
 	{
 		var thing = things[i];
 		if (thing.msgCD > 0)
 			thing.msgCD--;
 		thing.draw(ctx);
+	}
+	
+	for (var i in guns)
+	{
+		guns[i].draw(ctx);
 	}
 }
 
@@ -123,15 +175,19 @@ function collisionCheck()
 		var thing = things[i];
 		if (thing == currentThing)
 			continue;
+		
+		// GRAVITY //
 		// Needs to accelerate? down is positive
 		thing.vY += MULTIPLIER * INTERVAL / 1000;
 		
 		// Now actually add velocities onto the coords
 		thing.y += thing.vY * INTERVAL / 1000;
 		thing.x += thing.vX * INTERVAL / 1000;
-		if (thing.y + thing.size >= canvas.height)
+		
+		// WALL CHECKS //
+		if (thing.y + thing.size >= FLOORLEVEL)
 		{
-			thing.y = canvas.height - thing.size;
+			thing.y = FLOORLEVEL - thing.size;
 			thing.vY = -thing.vY * 0.5;
 			if (Math.abs(thing.vY) < 10)
 				thing.vY = 0;
@@ -148,17 +204,19 @@ function collisionCheck()
 		{
 			thing.x = canvas.width - thing.size;
 			thing.vX = -thing.vX * 0.8;
-			thing.say();
+			if (Math.abs(thing.vX) > 10)
+				thing.say();
 		}
 		else if (thing.x - thing.size <= 0)
 		{
 			thing.x = thing.size;
 			thing.vX = -thing.vX * 0.8;
-			thing.say();
+			if (Math.abs(thing.vX) > 10)
+				thing.say();
 		}
 		
 		// Friction! But only if on the ground
-		if (thing.y == canvas.height - thing.size)
+		if (thing.y == FLOORLEVEL - thing.size)
 		{
 			if (thing.vX > 0)
 			{
@@ -173,19 +231,72 @@ function collisionCheck()
 					thing.vX = 0;
 			}
 		}
+		
+		// COLLISION WITH OTHER OBJECTS
+		for (var j in things)
+		{
+			var thing2 = things[j];
+			if (i == j || thing.exclude.indexOf(thing2) != -1)
+				continue;
+			if (thing.collides(thing2))
+			{
+				console.log("collide");
+				thing.exclude.push(thing2);
+				
+				
+				var velX1 = (thing.vX * (thing.size - thing2.size) + (2 * thing2.size * thing2.vX)) / (thing.size + thing2.size);
+				var velY1 = (thing.vY * (thing.size - thing2.size) + (2 * thing2.size * thing2.vY)) / (thing.size + thing2.size);
+				var velX2 = (thing2.vX * (thing2.size - thing.size) + (2 * thing.size * thing.vX)) / (thing.size + thing2.size);
+				var velY2 = (thing2.vY * (thing2.size - thing.size) + (2 * thing.size * thing.vY)) / (thing.size + thing2.size);
+
+				thing.vX = velX1;
+				thing.vY = velY1;
+				thing2.vX = velX2;
+				thing2.vY = velY2;
+			}
+		}
+		
+		for (var j in things)
+		{
+			things[j].exclude = [];
+		}
 	}
 	update();
+}
+
+function getContext()
+{
+	var canvas = document.getElementById("canvas");
+	if (canvas.getContext)
+	{
+		var ctx = canvas.getContext("2d");
+		ctx.font = "14px sans-serif"
+		return ctx;
+	}
+	return "";
+}
+
+var chaos = function()
+{
+	var hrng = [-1, 1];
+	for (var i in things)
+	{
+		var thing = things[i];
+		thing.vX += hrng[Math.floor(Math.random() * 2)]*Math.floor(Math.random() * CHAOS + CHAOS);
+		thing.vY += hrng[Math.floor(Math.random() * 2)]*Math.floor(Math.random() * CHAOS + CHAOS);
+	}
 }
 
 //////////////////
 // THING OBJECT //
 //////////////////
-function Thing(x, y, size, color)
+function Thing(x, y, size, color, canSay)
 {
 	this.x = x;
 	this.y = y;
 	this.size = size;
 	this.color = color;
+	this.canSay = canSay
 	this.vX = 0;
 	this.vY = 0;
 	this.msg = "";
@@ -209,43 +320,53 @@ Thing.prototype.draw = function(ctx)
 
 Thing.prototype.contains = function(x, y)
 {
-	// just use square bounding box for now
-	if (this.x - this.size > x || this.x + this.size < x || this.y - this.size > y || this.y + this.size < y)
-		return false;
-	return true;
+	return (Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2) <= this.size * this.size);
 }
 
 Thing.prototype.say = function()
 {
+	if (!this.canSay)
+		return;
 	this.msg = messages[Math.floor(Math.random() * messages.length)];
 	this.msgCD = 100;
 	this.xOffset = Math.floor(Math.random() * this.size * 2);
 	this.yOffset = Math.floor(Math.random() * this.size * 2 - this.size)
 }
-//////////////////
 
-
-function getContext()
+Thing.prototype.collides = function(thing)
 {
-	var canvas = document.getElementById("canvas");
-	if (canvas.getContext)
-	{
-		var ctx = canvas.getContext("2d");
-		ctx.font = "14px sans-serif"
-		return ctx;
-	}
-	return "";
+	return (Math.pow(thing.x - this.x, 2) + Math.pow(thing.y - this.y, 2) < Math.pow(thing.size + this.size, 2));
 }
 
+//////////////////
 
-var chaos = function()
+////////////////
+// GUN OBJECT //
+////////////////
+function Gun(x, y, size)
 {
-	console.log("chaos");
-	var hrng = [-1, 1];
-	for (var i in things)
-	{
-		var thing = things[i];
-		thing.vX += hrng[Math.floor(Math.random() * 2)]*Math.floor(Math.random() * CHAOS + CHAOS);
-		thing.vY += hrng[Math.floor(Math.random() * 2)]*Math.floor(Math.random() * CHAOS + CHAOS);
-	}
+	this.x = x;
+	this.y = y;
+	this.size = size;
+}
+
+Gun.prototype.draw = function(ctx)
+{
+	ctx.fillStyle = "rgba(100,100,100,0.5)";
+	var path = new Path2D();
+	path.arc(this.x, this.y, this.size, 0, 360);
+	ctx.fill(path);
+}
+
+Gun.prototype.launch = function(vX, vY)
+{
+	var newThing = new Thing(this.x, this.y, 10, "rgba(100,100,100,0.8)", false);
+	newThing.vX = vX;
+	newThing.vY = vY;
+	things.push(newThing);
+}
+
+Gun.prototype.contains = function(x, y)
+{
+	return (Math.pow(x - this.x, 2) + Math.pow(y - this.y, 2) <= this.size * this.size);
 }
